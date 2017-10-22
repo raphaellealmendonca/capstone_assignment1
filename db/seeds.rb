1a02f2133e1acb1177b50e8c97bd68bb110f1764
Raphael <lealnet@gmail.com>
2017-10-22 12:18:46 -0200

    Added seed for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-cities = City.create([{ name: 'Baltimore' }])
-states = State.create([{ name: 'Maryland' }])
+@searchCity = City.find_by_name('Baltimore')
+if @searchCity.blank?
+  cities = City.create([{ name: 'Baltimore' }])
+end
 
+@searchState = State.find_by_name('Maryland')
+if @searchState.blank?
+  states = State.create([{ name: 'Maryland' }])
+end
