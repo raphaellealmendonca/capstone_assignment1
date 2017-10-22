cc056dc56581ebae2b5789f31bf5f0714abb29e1
Raphael <lealnet@gmail.com>
2017-10-22 12:47:24 -0200

    Added seed for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+@searchCity = City.find_by_name('Baltimore')
+if @searchCity.blank?
+  cities = City.create([{ name: 'Baltimore' }])
+end
+
+state = State.new(
+  name: "Maryland"
+)
+state.upsert
